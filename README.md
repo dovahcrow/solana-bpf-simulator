d19508b75b78f5aca9002db8629fb8faa777dd73
Weiyuan Wu <weiyuan@crows.land>
2023-12-10 19:43:07 -0800

    refactor

--- README.md
+++ README.md
@@ -1,17 +1,18 @@
-# Solana Simulator
+# Solana BPF Simulator
 Simulate a solana program locally without uploading it.
 
 ## Example
+Download the corresponding binary from the releases first.
 
 Get the program data:
 ```shell
-cargo make run --release --bin get_program_data -- --program GFXsSL5sSaDfNFQUYsHekbWBW1TsFdjDYzACh62tEHxn
+solana-bpf-simulator get-program-data --program GFXsSL5sSaDfNFQUYsHekbWBW1TsFdjDYzACh62tEHxn
 ```
 Or prepare the program data (e.g. the *.so from cargo build-sbf) by yourself.
 
 Then:
 ```shell
-cargo make run --release --bin simulate -- --program program.so --program-id GFXsSL5sSaDfNFQUYsHekbWBW1TsFdjDYzACh62tEHxn --instruction "fwjdYzA77n1" --account F451mjRqGEu1azbj46v4FuMEt1CacaPHQKUHzuTqKp4R --account 3NvDNxLa1AZofqmsfxRLswWUh1QqoWNQxm2C7azeaKFt --account 294A1PmDuLHrmgcPKHoQd9GgrJxQ6y2WkpPhSVgbkUoD --account Af6DjX1eRjfmnF1Lfe99FTgLUMKTVrsyf7CY2Kx9kzbT --account 7T9Y3aBkfUWNAocUijEJkzGBVG9CvawyqU7PuHE5RuDp --account oQZvi1vixbxer5XNrGHjwSHsrVeu3FFekw2oaV9DCGy --account 5Z2m2Hyc6S1Hmuee5HPnh2gWVGf4cHgFtzJWnRBV4zE7 --account H6ARHf6YXhGYeQfUzQNGk6rDNnLBQKrenN712K4AQJEG --account 5Yk3rUpMK2fANTkZtcD7dcHjsHC7KSBXLjDjvr3pKZsd --account 7yyaeuJ1GGtVBLT2z2xub5ZWYKaNhF28mj1RdV4VDFVk --account FdUm8MtCFGMC2UvxEV2bywKBQaP6es7osMwqZ9i2Gbvi --account 6kCU7GxqqUwuPMSrcXq5sXicoXqnA6KnsavzGcbZzRcg --account EoApD8hkDePGpfMwA9rpwwUGJuMZ7u2wK6z9z2pk7EoM --account TokenkegQfeZyiNwAJbNbGKPFXCWuBvf9Ss623VQ5DA --writable-account 3NvDNxLa1AZofqmsfxRLswWUh1QqoWNQxm2C7azeaKFt --writable-account 294A1PmDuLHrmgcPKHoQd9GgrJxQ6y2WkpPhSVgbkUoD --writable-account Af6DjX1eRjfmnF1Lfe99FTgLUMKTVrsyf7CY2Kx9kzbT --writable-account 7T9Y3aBkfUWNAocUijEJkzGBVG9CvawyqU7PuHE5RuDp --writable-account oQZvi1vixbxer5XNrGHjwSHsrVeu3FFekw2oaV9DCGy --writable-account 5Z2m2Hyc6S1Hmuee5HPnh2gWVGf4cHgFtzJWnRBV4zE7 --writable-account 5Yk3rUpMK2fANTkZtcD7dcHjsHC7KSBXLjDjvr3pKZsd --writable-account EoApD8hkDePGpfMwA9rpwwUGJuMZ7u2wK6z9z2pk7EoM
+solana-bpf-simulator simulate --program program.so --program-id GFXsSL5sSaDfNFQUYsHekbWBW1TsFdjDYzACh62tEHxn --instruction "fwjdYzA77n1" --account F451mjRqGEu1azbj46v4FuMEt1CacaPHQKUHzuTqKp4R --account 3NvDNxLa1AZofqmsfxRLswWUh1QqoWNQxm2C7azeaKFt --account 294A1PmDuLHrmgcPKHoQd9GgrJxQ6y2WkpPhSVgbkUoD --account Af6DjX1eRjfmnF1Lfe99FTgLUMKTVrsyf7CY2Kx9kzbT --account 7T9Y3aBkfUWNAocUijEJkzGBVG9CvawyqU7PuHE5RuDp --account oQZvi1vixbxer5XNrGHjwSHsrVeu3FFekw2oaV9DCGy --account 5Z2m2Hyc6S1Hmuee5HPnh2gWVGf4cHgFtzJWnRBV4zE7 --account H6ARHf6YXhGYeQfUzQNGk6rDNnLBQKrenN712K4AQJEG --account 5Yk3rUpMK2fANTkZtcD7dcHjsHC7KSBXLjDjvr3pKZsd --account 7yyaeuJ1GGtVBLT2z2xub5ZWYKaNhF28mj1RdV4VDFVk --account FdUm8MtCFGMC2UvxEV2bywKBQaP6es7osMwqZ9i2Gbvi --account 6kCU7GxqqUwuPMSrcXq5sXicoXqnA6KnsavzGcbZzRcg --account EoApD8hkDePGpfMwA9rpwwUGJuMZ7u2wK6z9z2pk7EoM --account TokenkegQfeZyiNwAJbNbGKPFXCWuBvf9Ss623VQ5DA --writable-account 3NvDNxLa1AZofqmsfxRLswWUh1QqoWNQxm2C7azeaKFt --writable-account 294A1PmDuLHrmgcPKHoQd9GgrJxQ6y2WkpPhSVgbkUoD --writable-account Af6DjX1eRjfmnF1Lfe99FTgLUMKTVrsyf7CY2Kx9kzbT --writable-account 7T9Y3aBkfUWNAocUijEJkzGBVG9CvawyqU7PuHE5RuDp --writable-account oQZvi1vixbxer5XNrGHjwSHsrVeu3FFekw2oaV9DCGy --writable-account 5Z2m2Hyc6S1Hmuee5HPnh2gWVGf4cHgFtzJWnRBV4zE7 --writable-account 5Yk3rUpMK2fANTkZtcD7dcHjsHC7KSBXLjDjvr3pKZsd --writable-account EoApD8hkDePGpfMwA9rpwwUGJuMZ7u2wK6z9z2pk7EoM
 ```
 
 ## Note
---
+++ src/main.rs
@@ -0,0 +1,255 @@
+use std::{
+    collections::{hash_map, HashMap},
+    fs::{File, OpenOptions},
+    io::{Read, Write},
+    path::PathBuf,
+};
+
+use anyhow::{anyhow, Error};
+use clap::{Parser, Subcommand};
+use fehler::{throw, throws};
+use once_cell::sync::Lazy;
+use solana_bpf_simulator::{SBFExecutor, WorkingSlot, FEATURES};
+use solana_client::rpc_client::RpcClient;
+use solana_sdk::{
+    account::{Account, AccountSharedData, ReadableAccount},
+    bpf_loader_upgradeable::UpgradeableLoaderState,
+    clock::Clock,
+    instruction::{AccountMeta, Instruction},
+    message::{LegacyMessage, Message, SanitizedMessage},
+    native_loader, pubkey,
+    pubkey::Pubkey,
+    slot_history::Slot,
+    sysvar::clock,
+};
+use tracing::{error, info};
+use tracing_subscriber::{
+    filter::{EnvFilter, LevelFilter},
+    fmt,
+    prelude::*,
+    Registry,
+};
+use url::Url;
+
+static BPF_LOADER: Lazy<AccountSharedData> = Lazy::new(|| {
+    Account {
+        owner: native_loader::ID,
+        executable: true,
+        rent_epoch: 0,
+        data: b"solana_bpf_loader_upgradeable_program".to_vec(),
+        lamports: 1,
+    }
+    .into()
+});
+
+#[derive(Parser)]
+struct Cli {
+    #[arg(long, env, default_value = "https://api.mainnet-beta.solana.com")]
+    solana_rpc: Url,
+
+    #[command(subcommand)]
+    command: Command,
+}
+
+#[derive(Subcommand)]
+enum Command {
+    Simulate(Simulate),
+    GetProgramData(GetProgramData),
+}
+
+#[throws(Error)]
+fn main() {
+    let cli = Cli::parse();
+    let subscriber = Registry::default()
+        .with(
+            fmt::layer()
+                .with_writer(std::io::stdout)
+                .with_filter(LevelFilter::INFO),
+        )
+        .with(EnvFilter::builder().try_from_env().unwrap_or_else(|_| {
+            EnvFilter::builder()
+                .parse("solana_bpf_simulator=info")
+                .unwrap()
+        }));
+
+    tracing::subscriber::set_global_default(subscriber).unwrap();
+
+    let rpc = RpcClient::new(cli.solana_rpc.to_string());
+
+    match cli.command {
+        Command::Simulate(c) => c.run(&rpc)?,
+        Command::GetProgramData(c) => c.run(&rpc)?,
+    }
+}
+
+#[derive(Debug, Clone, Parser)]
+struct Simulate {
+    #[arg(long)]
+    program_id: Pubkey,
+
+    #[arg(long, default_value = "program.so")]
+    program: PathBuf,
+
+    #[arg(long)]
+    instruction: String, // base58 string
+
+    #[arg(long)]
+    account: Vec<Pubkey>,
+
+    #[arg(long)]
+    signer_account: Vec<Pubkey>,
+
+    #[arg(long)]
+    writable_account: Vec<Pubkey>,
+}
+
+impl Simulate {
+    #[throws(Error)]
+    fn run(&self, rpc: &RpcClient) {
+        let mut sbf = SBFExecutor::new(FEATURES).unwrap();
+
+        let clock = rpc.get_account(&clock::id())?;
+        let clock: Clock = bincode::deserialize(&clock.data())?;
+
+        let slot = clock.slot;
+        sbf.sysvar_cache_mut().set_clock(clock);
+
+        let bpf_upgradable_loader = pubkey!("BPFLoaderUpgradeab1e11111111111111111111111");
+        let programdata_address = pubkey!("DUMMYPRoGRAMDATA111111111111111111111111111");
+
+        let program: AccountSharedData = Account {
+            lamports: 0,
+            data: bincode::serialize(&UpgradeableLoaderState::Program {
+                programdata_address,
+            })?,
+            owner: bpf_upgradable_loader,
+            executable: true,
+            rent_epoch: 0,
+        }
+        .into();
+
+        let mut data = vec![];
+        File::open(&self.program)?.read_to_end(&mut data)?;
+
+        let program_data: AccountSharedData = Account {
+            lamports: 0,
+            data,
+            owner: bpf_upgradable_loader,
+            executable: true,
+            rent_epoch: 0,
+        }
+        .into();
+
+        let mut accounts: HashMap<Pubkey, AccountSharedData> = HashMap::new();
+        let mut loader = sbf.loader(|&key| {
+            if key == pubkey!("BPFLoaderUpgradeab1e11111111111111111111111") {
+                return Some(BPF_LOADER.clone());
+            }
+
+            if key == self.program_id {
+                return Some(program.clone());
+            }
+
+            if key == programdata_address {
+                return Some(program_data.clone());
+            }
+
+            match accounts.entry(key) {
+                hash_map::Entry::Occupied(e) => return Some(e.get().clone()),
+                hash_map::Entry::Vacant(e) => {
+                    let account = rpc.get_account(&key).unwrap_or_default();
+                    let account: AccountSharedData = account.into();
+                    e.insert(account.clone());
+                    return Some(account);
+                }
+            }
+        });
+
+        let ix_data = bs58::decode(&self.instruction).into_vec()?;
+        let ix = Instruction::new_with_bytes(
+            self.program_id,
+            &ix_data,
+            self.account
+                .iter()
+                .map(|a| {
+                    let mut signer = false;
+                    let mut writable = false;
+
+                    if self.signer_account.contains(a) {
+                        signer = true;
+                    }
+
+                    if self.writable_account.contains(a) {
+                        writable = true;
+                    }
+
+                    AccountMeta {
+                        pubkey: *a,
+                        is_signer: signer,
+                        is_writable: writable,
+                    }
+                })
+                .collect(),
+        );
+        let message = Message::new(&[ix], None);
+        let message = SanitizedMessage::Legacy(LegacyMessage::new(message));
+        let loaded_transaction = loader.load_transaction_account(&message)?;
+        let loaded_programs = loader.load_programs(&WrappedSlot(slot), [&message])?;
+
+        sbf.record_log();
+        let res = sbf.process(slot, &message, loaded_transaction, &loaded_programs);
+
+        if let Err(e) = res {
+            error!(
+                "Invoke errored: {}:\nLogs: {:?}",
+                e,
+                sbf.logger().get_recorded_content()
+            );
+        } else {
+            info!("{:?}", sbf.logger().get_recorded_content());
+        }
+    }
+}
+
+#[derive(Debug, Clone, Parser)]
+struct GetProgramData {
+    #[arg(long)]
+    program: Pubkey,
+
+    #[arg(long, default_value = "program.so")]
+    dest: PathBuf,
+}
+
+impl GetProgramData {
+    #[throws(Error)]
+    fn run(&self, rpc: &RpcClient) {
+        let acc = rpc.get_account(&self.program)?;
+        let state: UpgradeableLoaderState = bincode::deserialize(acc.data())?;
+
+        let address = match state {
+            UpgradeableLoaderState::Program {
+                programdata_address,
+            } => programdata_address,
+            _ => throw!(anyhow!("Wrong state")),
+        };
+        let acc = rpc.get_account(&address)?;
+
+        let mut f = OpenOptions::new()
+            .create(true)
+            .write(true)
+            .truncate(true)
+            .open(&self.dest)?;
+        f.write_all(acc.data())?;
+    }
+}
+
+struct WrappedSlot(Slot);
+impl WorkingSlot for WrappedSlot {
+    fn current_slot(&self) -> Slot {
+        self.0
+    }
+
+    fn is_ancestor(&self, _: Slot) -> bool {
+        true
+    }
+}
