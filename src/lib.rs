4bcb7cb672f7ac5969c4509db461be442dc007c1
Weiyuan Wu <weiyuan@crows.land>
2024-05-22 01:43:13 -0700

    solana 1.16

--- src/lib.rs
+++ src/lib.rs
@@ -1,9 +1,5 @@
 #[cfg(feature = "instruction")]
 mod instruction_executor;
-#[cfg(feature = "message")]
-mod message_executor;
 
 #[cfg(feature = "instruction")]
 pub use instruction_executor::*;
-#[cfg(feature = "message")]
-pub use message_executor::*;
